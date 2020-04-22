c8d34ef57f531f8b39bb5a21d6022bf1cc068e9e
sakshamtaneja21 <sakshamtaneja7861@gmail.com>
2020-04-22 07:42:55 +0530

    crash course on python by google

---
+++ programes/crash course on python/Week 1/print.py
@@ -0,0 +1,3 @@
+#Write a Python script that outputs "Automating with Python is fun!" to the screen.
+
+print("Automating with Python is fun!")
---
+++ programes/crash course on python/Week 1/sec_calculator.py
@@ -0,0 +1,9 @@
+#Keeping in mind there are 86400 seconds per day, write a program that calculates how many seconds there are in a week, if a week is 7 days. Print the result on the screen.
+
+#Note: Your result should be in the format of just a number, not a sentence.
+
+secInMin = 60
+secInHour = 60 * secInMin
+secInDay = 24 * secInHour
+secInWeek = 7 * secInDay
+print(secInWeek)
